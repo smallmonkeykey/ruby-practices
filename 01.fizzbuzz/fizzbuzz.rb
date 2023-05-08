483b11044d69ddaf4d69165f7a79dd049fff7b71
smallmonkeykey <sarinitakuoo@gmail.com>
2023-05-08 16:12:43 +0900

    インデントを修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,13 @@
 numbers = [*1..20]
 numbers.each do |number|
-    if number % 15 == 0
-        puts "FizzBuzz"
-    elsif number % 5 == 0
-        puts "Buzz"
-    elsif number % 3 == 0
-        puts "Fizz"
-    else
-        puts number
-    end
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  else
+    puts number
+  end
 end
+
