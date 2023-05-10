632118e70cc478d624511c43f1f097a5d1abc3ca
smallmonkeykey <sarinitakuoo@gmail.com>
2023-05-10 14:12:56 +0900

    新ボーリングスコアのプログラムを追記™

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X' # strike
+    shots << 10 << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+point = 0
+frames.each do |frame|
+  if frame[0] == 10 # strike
+    point += 30
+  elsif frame.sum == 10 # spare
+    point += frame[0] + 10
+  else
+    point += frame.sum
+  end
+end
+puts point
