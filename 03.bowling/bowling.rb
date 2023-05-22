d99294712cddddb0068c2871099c9d33fc7ff562
smallmonkeykey <sarinitakuoo@gmail.com>
2023-05-22 14:16:13 +0900

    10本のピンを定数化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
+ALL_PINS = 10
 
 score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  s == 'X' ? shots << 10 << 0 : shots << s.to_i
+  s == 'X' ? shots << ALL_PINS << 0 : shots << s.to_i
 end
 
 frames = []
 shots.each_slice(2) do |s|
-  s.delete(0) if s[0] == 10
+  s.delete(0) if s[0] == ALL_PINS
   frames << s
 end
 
@@ -18,17 +19,17 @@ point = 0
 frames.each.with_index do |frame, i|
   point += frame.sum
 
-  if frame[0] == 10 && i < 9
-    if frames[i + 1] == [10] && frames[i + 2] == [10]
+  if frame[0] == ALL_PINS && i < 9
+    if frames[i + 1] == [ALL_PINS] && frames[i + 2] == [ALL_PINS]
       point += 20
-    elsif frames[i + 1] == [10]
-      point += 10 + frames[i + 2][0]
+    elsif frames[i + 1] == [ALL_PINS]
+      point += ALL_PINS + frames[i + 2][0]
     else
       point += frames[i + 1].sum
     end
   end
 
-  if frame.sum == 10 && frame.size == 2 && i < 9
+  if frame.sum == ALL_PINS && frame.size == 2 && i < 9
     point += frames[i + 1][0]
   end
 end
