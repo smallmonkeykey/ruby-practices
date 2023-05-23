60b2656e5f10020fde56e386911b1e519d73c792
smallmonkeykey <sarinitakuoo@gmail.com>
2023-05-23 21:32:15 +0900

    結果を横並びに表示

---
+++ 04.ls/ls.rb
@@ -0,0 +1,17 @@
+pwd_dir = []
+
+Dir.open('.').each_child {|f|
+  if File.ftype(f) == "directory"
+    pwd_dir << "#{f}/"
+  else
+  pwd_dir << f 
+  end
+}
+
+pwd_dir.sort.each_with_index do |n, idx|
+  idx += 1
+  print n.ljust(30)
+  print "\n"  if idx % 3 == 0
+ end
+
+puts ""
