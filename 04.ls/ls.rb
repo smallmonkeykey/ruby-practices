ec6236d0b66bfc3049dd7b5295a713b9bb0d14f9
smallmonkeykey <sarinitakuoo@gmail.com>
2023-05-30 14:41:38 +0900

    rubocopで修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,19 +1,21 @@
-def get_dirs
+# frozen_string_literal: true
+
+def take_dirs
   pwd_dirs = []
-  Dir.open('.').each_child {|file|
-    if File.ftype(file) == "directory"
-      pwd_dirs << "#{file}/"
-    else
-      pwd_dirs << file
-    end
-  }
+  Dir.open('.').each_child do |file|
+    pwd_dirs << if File.ftype(file) == 'directory'
+                  "#{file}/"
+                else
+                  file
+                end
+  end
   pwd_dirs
 end
 
-sort_pwd_dirs = get_dirs.sort
+sort_pwd_dirs = take_dirs.sort
 
 def fill_up(sort_pwd_dirs, number)
-  number - (sort_pwd_dirs.size.modulo(number))
+  number - sort_pwd_dirs.size.modulo(number)
 end
 
 quotient = sort_pwd_dirs.size.div(3)
@@ -22,13 +24,11 @@ columns = quotient + 1
 arry = []
 sort_pwd_dirs.each_slice(columns).with_index(1) do |dir, idx|
   arry << dir
-  if idx == quotient 
-    fill_up(sort_pwd_dirs, 3).times {arry.last.push("")}
-  end
+  fill_up(sort_pwd_dirs, 3).times { arry.last.push('') } if idx == quotient
 end
 
 reversed_array = arry.transpose
 
 reversed_array.each do |line|
-  puts line.map{|file| file.ljust(24)}.join()
+  puts line.map { |file| file.ljust(24) }.join
 end
