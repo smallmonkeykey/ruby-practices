f52b2a76a7a2667b4662ff1330ee316f288f3a72
smallmonkeykey <sarinitakuoo@gmail.com>
2023-06-13 14:52:11 +0900

    rubocopで修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
-pwd_dirs_files = Dir.glob("*") 
+pwd_dirs_files = Dir.glob('*')
 
 ROWS = 3
-
-arry = []
 
 arry = pwd_dirs_files.each_slice((pwd_dirs_files.size.to_f / ROWS).ceil).to_a
 
 def add_space(arry)
-  (arry[0].size - arry.last.size).times { arry.last.push(' ') } 
+  (arry[0].size - arry.last.size).times { arry.last.push(' ') }
 end
 
 add_space(arry)
 
-arry_size = pwd_dirs_files.map do |file|
-  file.size               
-end
+arry_size = pwd_dirs_files.map(&:size)
 
 def transpose_arry(arry, arry_size)
   arry.transpose.each do |line|
