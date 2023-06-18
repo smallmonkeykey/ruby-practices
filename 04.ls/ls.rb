5c74a6a09f5182f50b50d4e591e1dd6d07960a4f
smallmonkeykey <sarinitakuoo@gmail.com>
2023-06-18 18:20:49 +0900

    変数名をfilenamesに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-pwd_dirs_files = Dir.glob('*')
+filenames = Dir.glob('*')
 
 ROWS = 3
 
-arry = pwd_dirs_files.each_slice((pwd_dirs_files.size.to_f / ROWS).ceil).to_a
+arry = filenames.each_slice((filenames.size.to_f / ROWS).ceil).to_a
 
 def add_space(arry)
   (arry[0].size - arry.last.size).times { arry.last.push(' ') }
@@ -12,7 +12,7 @@ end
 
 add_space(arry)
 
-arry_size = pwd_dirs_files.map(&:size)
+arry_size = filenames.map(&:size)
 
 def transpose_arry(arry, arry_size)
   arry.transpose.each do |line|
