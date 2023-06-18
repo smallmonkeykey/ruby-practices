f99195b2d1b168659f84e1436574e5330c5a7f87
smallmonkeykey <sarinitakuoo@gmail.com>
2023-06-18 18:25:35 +0900

    arryをdivided_arryに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,20 +4,20 @@ filenames = Dir.glob('*')
 
 ROWS = 3
 
-arry = filenames.each_slice((filenames.size.to_f / ROWS).ceil).to_a
+divided_arry = filenames.each_slice((filenames.size.to_f / ROWS).ceil).to_a
 
-def add_space(arry)
-  (arry[0].size - arry.last.size).times { arry.last.push(' ') }
+def add_space(divided_arry)
+  (divided_arry[0].size - divided_arry.last.size).times { divided_arry.last.push(' ') }
 end
 
-add_space(arry)
+add_space(divided_arry)
 
-arry_size = filenames.map(&:size)
+divided_arry_size = filenames.map(&:size)
 
-def transpose_arry(arry, arry_size)
-  arry.transpose.each do |line|
-    puts line.map { |file| file.ljust(arry_size.max + 1) }.join
+def transpose_divided_arry(divided_arry, divided_arry_size)
+  divided_arry.transpose.each do |line|
+    puts line.map { |file| file.ljust(divided_arry_size.max + 1) }.join
   end
 end
 
-transpose_arry(arry, arry_size)
+transpose_divided_arry(divided_arry, divided_arry_size)
