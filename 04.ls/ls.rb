c24aa1e206c9057b41a78b512b6d35a3df0e2c3e
smallmonkeykey <sarinitakuoo@gmail.com>
2023-07-07 13:12:55 +0900

    メソッド名をchunk_filenamesに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -20,7 +20,7 @@ filenames = get_filenames(params)
 
 ROWS = 3
 
-def split_files_equally(filenames)
+def chunk_filenames(filenames)
   sliced_filenames = filenames.each_slice((filenames.size.to_f / ROWS).ceil).to_a
   (sliced_filenames[0].size - sliced_filenames.last.size).times { sliced_filenames.last.push(' ') }
   sliced_filenames
@@ -28,7 +28,7 @@ end
 
 def transpose_split_files(filenames)
   arry_max_size = filenames.map(&:size).max
-  split_files_equally(filenames).transpose.each do |line|
+  chunk_filenames(filenames).transpose.each do |line|
     puts line.map { |file| file.ljust(arry_max_size + 1) }.join
   end
 end
