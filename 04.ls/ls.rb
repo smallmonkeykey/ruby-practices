82ace0027528c45db7bda38deb75a5daf4454b56
smallmonkeykey <sarinitakuoo@gmail.com>
2023-07-18 17:07:01 +0900

    オプションlを追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 opt = OptionParser.new
 params = {}
-opt.on('-r') { |v| v }
+opt.on('-l') { |v| v }
 
 opt.parse!(ARGV, into: params)
 
-def get_filenames(params)
-  params[:r] ? Dir.glob('*').reverse : Dir.glob('*')
+def determine_option(params)
+  params[:l] ? exec_ls_l_command : exec_ls_command
 end
 
 ROWS = 3
@@ -27,9 +28,73 @@ def display_filenames(filenames)
   end
 end
 
-def exec_ls_command(params)
-  filenames = get_filenames(params)
+def exec_ls_command
+  filenames = Dir.glob('*') 
   display_filenames(filenames)
 end
 
-exec_ls_command(params)
+
+def convert_filetypes(filetype)
+  {
+    "file" => "-",
+    "directory" => "d",
+    "characterSpecial" => "c",
+    "blockSpecial" => "b",
+    "link" => "l"
+  }[filetype]
+end
+
+def convert_permissions(filemode_number)
+    {
+    0 => "---",
+    1 => "--x",
+    2 => "-w-",
+    3 => "-wx",
+    4 => "r--",
+    5 => "r-x",
+    6 => "rw-",
+    7 => "rwx"
+  }[filemode_number]
+end
+
+def determine_permissions(filemode)
+  
+  last_three_digits = filemode.to_i % 1000
+  last_three_digits_arry = last_three_digits.digits
+
+  permission = last_three_digits_arry.map { |digit|convert_permissions(digit)}
+  permission.join("")
+end
+
+def display_ls_l_command(filenames)
+filenames.each do |filename|
+  filetype = File.ftype(filename)
+  filetype_result =  convert_filetypes(filetype)
+
+   stat = File.stat(filename)
+    filemode = stat.mode.to_s(8)
+    permission_result = determine_permissions(filemode)
+
+   hard_link = stat.nlink.to_s.rjust(3)
+
+   owner = Etc.getpwuid(stat.uid).name
+   group = Etc.getgrgid(stat.gid).name
+
+   file_size = stat.size.to_s.rjust(6)
+
+    time = stat.mtime.strftime("%_m %_d %H:%M")
+
+    print "#{filetype_result}#{permission_result}#{hard_link} #{owner}  #{group}#{file_size} #{time} #{filename}\n"
+ end
+
+end
+
+
+def exec_ls_l_command
+  filenames = Dir.glob('*') 
+  display_ls_l_command(filenames)
+end
+
+determine_option(params)
+
+
