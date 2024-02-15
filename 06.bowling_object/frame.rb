562e29b7b04a89bc3cf46c87635c6a72fb004aa1
smallmonkeykey <sarinitakuoo@gmail.com>
2024-02-15 13:38:17 +0900

    normal_scoreからscore_without_bonusに変更

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -13,7 +13,7 @@ class Frame
     @third_shot = Shot.new(third_mark)
   end
 
-  def normal_score
+  def score_without_bonus
     score = first_shot.score + second_shot.score
     score += @third_shot.score if @third_shot.score != 0
     score
