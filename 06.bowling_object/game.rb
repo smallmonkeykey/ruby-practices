d3be5eb6c72693207206c86f64083d708999320c
smallmonkeykey <sarinitakuoo@gmail.com>
2024-02-15 22:35:56 +0900

    calculate_normal_frameをFrameクラスに移動

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -27,18 +27,11 @@ class Game
 
   def calculate_score
     totals = []
-    split_frames.each_with_index { |frame, index| totals << (index < 9 ? calculate_normal_frame(frame, index) : frame.score_without_bonus) }
+    split_frames.each_with_index do |frame, index|
+      next_frame = @frames[index + 1]
+      next_next_frame = @frames[index + 2]
+      totals << (index < 9 ? frame.calculate_normal_frame(next_frame, next_next_frame) : frame.score_without_bonus)
+    end
     totals.sum
-  end
-
-  private
-
-  def calculate_normal_frame(frame, index)
-    total = 0
-    total += frame.score_without_bonus
-    total += @frames[index + 1].first_shot.score if frame.strike? || frame.spare?
-    total += @frames[index + 1].second_shot.score if frame.strike?
-    total += @frames[index + 2].first_shot.score if index < 8 && frame.strike? && @frames[index + 1].strike?
-    total
   end
 end
