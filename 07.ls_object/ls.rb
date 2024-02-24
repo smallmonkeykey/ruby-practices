53efbd774308d0c11c53e7ed881ca7b1d6fa1c29
smallmonkeykey <sarinitakuoo@gmail.com>
2024-02-24 22:37:35 +0900

    rubocopで修正

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,15 +1,15 @@
-
+# frozen_string_literal: true
 
 class Ls
   NUMBER_OF_COLUMNS = 3
 
   def initialize
-    @filenames = Dir.glob("*").sort
+    @filenames = Dir.glob('*').sort
   end
 
   def list_files
     create_arranged_filenames.each do |row|
-        puts row.map { |file| file.ljust(arry_max_size + 2) }.join
+      puts row.map { |file| file.ljust(arry_max_size + 2) }.join
     end
   end
 
@@ -17,18 +17,17 @@ class Ls
     arranged_filenames = []
     column_size = (@filenames.size - 1) / NUMBER_OF_COLUMNS + 1
     column_size.times do |i|
-        row = []
-        NUMBER_OF_COLUMNS.times do
+      row = []
+      NUMBER_OF_COLUMNS.times do
         row << @filenames[i]
-         i += column_size
-        end
-        arranged_filenames << row.compact
+        i += column_size
+      end
+      arranged_filenames << row.compact
     end
     arranged_filenames
   end
 
   def arry_max_size
-   @filenames.map(&:size).max
+    @filenames.map(&:size).max
   end
-
 end
