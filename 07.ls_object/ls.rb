e3fbd8da0a6ee6b10a1443106d36c2215efc69a5
smallmonkeykey <sarinitakuoo@gmail.com>
2024-02-24 19:40:32 +0900

    Lsクラスを作成

---
+++ 07.ls_object/ls.rb
@@ -0,0 +1,34 @@
+
+
+class Ls
+  NUMBER_OF_COLUMNS = 3
+
+  def initialize
+    @filenames = Dir.glob("*").sort
+  end
+
+  def list_files
+    create_arranged_filenames.each do |row|
+        puts row.map { |file| file.ljust(arry_max_size + 2) }.join
+    end
+  end
+
+  def create_arranged_filenames
+    arranged_filenames = []
+    column_size = (@filenames.size - 1) / NUMBER_OF_COLUMNS + 1
+    column_size.times do |i|
+        row = []
+        NUMBER_OF_COLUMNS.times do
+        row << @filenames[i]
+         i += column_size
+        end
+        arranged_filenames << row.compact
+    end
+    arranged_filenames
+  end
+
+  def arry_max_size
+   @filenames.map(&:size).max
+  end
+
+end
