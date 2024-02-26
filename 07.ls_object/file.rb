8b096f8eaaedf325cf8f01fbd754ede2b7dd9540
smallmonkeykey <sarinitakuoo@gmail.com>
2024-02-26 22:44:48 +0900

    オプション別のfilenamesクラス(Fileクラス)を作成

---
+++ 07.ls_object/file.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class File
+  require 'optparse'
+
+  def initialize(params)
+    @params = params
+  end
+
+  def create_filenames
+   filenames = Dir.glob('*')
+   filenames = Dir.entries('.').sort if @params[:a]
+   filenames = filenames.reverse if @params[:r]
+   filenames
+  end
+end
--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
+require 'optparse'
+require_relative 'file'
 
 class Ls
   NUMBER_OF_COLUMNS = 3
 
-  def initialize
-    @filenames = Dir.glob('*').sort
+  def initialize(params)
+    @params = params
+    file = File.new(@params)
+    @filenames = file.create_filenames
   end
 
-  def list_files
-    create_arranged_filenames.each do |row|
+	def list_files
+		@params["l"] ? longformat : short_format
+	end
+
+  def short_format
+			create_arranged_filenames.each do |row|
       puts row.map { |file| file.ljust(max_size + 2) }.join
     end
   end
