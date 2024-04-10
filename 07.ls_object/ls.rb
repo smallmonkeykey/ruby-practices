3789368c1e0a168cd2b5fc78f4dd2903481c129e
smallmonkeykey <sarinitakuoo@gmail.com>
2024-04-10 18:20:03 +0900

    ShortFormatクラスを作成

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require_relative 'filepath'
 require_relative 'longformat'
+require_relative 'shortformat'
 
 class Ls
-  NUMBER_OF_COLUMNS = 3
-
   def initialize(options)
     @options = options
     @filenames = create_filenames
   end
 
   def list_files
-    @options['l'] ? LongFormat.new(@filenames).list_files : short_format
+    @options['l'] ? LongFormat.new(@filenames).list_files : ShortFormat.new(@filenames).list_files
   end
 
   private
@@ -23,29 +21,5 @@ class Ls
     filenames = Dir.entries('.').sort if @options['a']
     filenames = filenames.reverse if @options['r']
     filenames
-  end
-
-  def short_format
-    create_arranged_filenames.each do |row|
-      puts row.map { |file| file.ljust(max_size + 2) }.join
-    end
-  end
-
-  def create_arranged_filenames
-    arranged_filenames = []
-    column_size = (@filenames.size - 1) / NUMBER_OF_COLUMNS + 1
-    column_size.times do |i|
-      row = []
-      NUMBER_OF_COLUMNS.times do
-        row << @filenames[i]
-        i += column_size
-      end
-      arranged_filenames << row.compact
-    end
-    arranged_filenames
-  end
-
-  def max_size
-    @filenames.map(&:size).max
   end
 end
